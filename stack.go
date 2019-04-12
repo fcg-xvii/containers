5c1c663f103e77309be15688216e9069b8742089
Vladimir V Maksimov <flint@77i.su>
2019-04-12 16:55:14 +0300

    Added comments in stack.go

--- stack.go
+++ stack.go
@@ -2,10 +2,12 @@ package containers
 
 import "fmt"
 
+// Структура стека
 type Stack struct {
 	list []interface{}
 }
 
+// Возвращает объект, находящийся на вершине стека
 func (s *Stack) Peek() interface{} {
 	if len(s.list) > 0 {
 		return s.list[len(s.list)-1]
@@ -13,10 +15,12 @@ func (s *Stack) Peek() interface{} {
 	return nil
 }
 
+// Помещает объект на вершину стека
 func (s *Stack) Push(vals ...interface{}) {
 	s.list = append(s.list, vals...)
 }
 
+// Забирает объект с вершины стека и возвращает его
 func (s *Stack) Pop() (res interface{}) {
 	if len(s.list) > 0 {
 		index := len(s.list) - 1
@@ -26,14 +30,17 @@ func (s *Stack) Pop() (res interface{}) {
 	return
 }
 
+// Возвращает размер стека
 func (s *Stack) Len() int {
 	return len(s.list)
 }
 
+// Возвращает вместимость структуры стека
 func (s *Stack) Cap() int {
 	return cap(s.list)
 }
 
+// Возвращает строку для предсавления элементов стека
 func (s *Stack) String() string {
 	res := fmt.Sprintf("Stack (len %v, cap %v)\n=====\n", len(s.list), cap(s.list))
 	for i := len(s.list) - 1; i >= 0; i-- {
@@ -43,18 +50,21 @@ func (s *Stack) String() string {
 	return res
 }
 
+// Забирает весь стек и возвращает его элементы в срезе
 func (s *Stack) PopAll() []interface{} {
 	res := s.list
 	s.list = s.list[:0]
 	return res
 }
 
+// Забирает стек до индекса (если в стеке, например, 5 элементов, индекс = 2, из стека будет взято 3 элемента)
 func (s *Stack) PopAllIndex(index int) []interface{} {
 	res := s.list[index:]
 	s.list = s.list[:index]
 	return res
 }
 
+// Разворачивает стек, забирает все элементы и возвращает их в срезе
 func (s *Stack) PopAllReverse() []interface{} {
 	res := make([]interface{}, len(s.list))
 	for i := 0; i < len(s.list); i++ {
@@ -64,6 +74,7 @@ func (s *Stack) PopAllReverse() []interface{} {
 	return res
 }
 
+// Забирает элементы из стека до индекса, разворачивает результат и возвращает его в срезе
 func (s *Stack) PopAllReverseIndex(index int) []interface{} {
 	res := make([]interface{}, len(s.list)-index)
 	for i := 0; i < len(s.list)-index; i++ {
