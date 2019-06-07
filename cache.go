6197421afc779b699f0430d729c528f0ac43cb4c
Vladimir V Maksimov <flint@77i.su>
2019-06-07 14:47:26 +0300

    LockedLoad updated

--- cache.go
+++ cache.go
@@ -178,7 +178,7 @@ func (s *cache) Len() int {
 	return res
 }
 
-func (s *cache) LockedLoad(callback func(CacheCallBacks) (interface{}, bool)) (item interface{}, check bool) {
+func (s *cache) LockedLoad(key interface{}, callback func(CacheCallBacks) (interface{}, bool)) (item interface{}, check bool) {
 	s.locker.Lock()
 	if item, check = callback(CacheCallBacks{s.set, s.delete, s.get}); check {
 		s.items[key] = &cacheItem{item, time.Now().Add(s.expired).UnixNano()}
